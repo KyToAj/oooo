374cae5b76d7a864430fd1ef6d31de9fc0097240
qiuqiuqiu <3267741280@qq.com>
2023-10-10 11:45:45 +0800

    Update Exception01.java

--- src/com/studyJava/exception/Exception01.java
+++ src/com/studyJava/exception/Exception01.java
@@ -8,9 +8,10 @@ public class Exception01 {
     public static void main(String[] args) {
         int num1 = 10;
         int num2 = 0;
+        int num3 = -15;
 
         try {
-            int result = num1/num2;
+            int result = num1/num2 + num3;
         } catch (Exception e) {
             e.printStackTrace();
         }
