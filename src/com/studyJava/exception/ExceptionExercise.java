bf26f8f23f5ac3392650aa9de0c0d11d7e03a05e
qiuqiuqiu <3267741280@qq.com>
2023-10-11 13:41:04 +0800

    Update ExceptionExercise.java

--- src/com/studyJava/exception/ExceptionExercise.java
+++ src/com/studyJava/exception/ExceptionExercise.java
@@ -7,15 +7,16 @@ import java.util.Scanner;
  * @version 1.0
  */
 public class ExceptionExercise {
-    public static int  s;
     public static void main(String[] args) {
+        int s = 0;
         boolean loop = true;
-
+        String inputStr = " ";
+        Scanner sca = new Scanner(System.in);
            while(loop){
                try {
                    System.out.println("请输入一个整数");
-                   Scanner sca = new Scanner(System.in);
-                   s = sca.nextInt();
+                   inputStr = sca.next();
+                   s = Integer.parseInt(inputStr);
                    loop = false;
                } catch (Exception e) {
                    System.out.println("输入的不是整数，请重新输入");
