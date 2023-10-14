88c99b20cf3dc2e8017b6c4a6624c9ffb76010f8
qiuqiuqiu <3267741280@qq.com>
2023-10-14 10:58:20 +0800

    src/

--- src/com/studyJava/Math_.java
+++ src/com/studyJava/Math_.java
@@ -18,5 +18,15 @@ public class Math_ {
         //返回最接近参数的整数
         double n4 = Math.round(5.4);
         System.out.println(n4);
+        //返回两个数中的较大值或较小值
+        double n5 = Math.max(n1,n3);
+        double n6 = Math.min(n1,n3);
+        System.out.println("max = " + n5 + ",min = " + n6);
+        //求幂
+        double n7 = Math.pow(2.5,6);
+        System.out.println(n7);
+        //求算数平方根
+        double n8 = Math.sqrt(n7);
+        System.out.println(n8);
     }
 }
